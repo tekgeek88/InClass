9e47a62849f1e5f5666845c4066141c8372e0127
tekgeek88 <tekgeek88@yahoo.com>
2015-02-02 19:14:54 -0800

    Fixed array for one die only and added percentages

---
+++ MyProgram.java
@@ -0,0 +1,16 @@
+
+public class MyProgram {
+
+	   public static void main(String[] args){
+	      double x = 1.0;
+	      double y = 4.0;
+	      double r;
+
+	     r = ToolBox.fun( x,  y );
+
+	      System.out.println("x = " + x);
+	      System.out.println("y = " + y);
+	      System.out.println("r = " + r);
+	   
+	} // end of main
+}
---
+++ ToolBox.java
@@ -0,0 +1,14 @@
+
+class ToolBox     
+{
+   public static double fun ( double a, double b )
+   {
+      double m = 0;
+
+      a = a + 1;
+      b = b + 2;
+      m = a + b;
+
+      return(m);   // Output of the method
+   }
+}
